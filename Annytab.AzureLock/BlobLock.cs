7d3b7b9982fbc6070ac1214521ae49b544c0876b
Fredrik Stigsson <info@annytab.se>
2016-10-27 13:20:22 +0200

    Initial commit

---
+++ Annytab.AzureLock/BlobLock.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Threading;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+
+namespace Annytab.AzureLock
+{
+    /// <summary>
+    /// This class is used to aquire, renew and release blob locks
+    /// </summary>
+    public class BlobLock : IDisposable
+    {
+        #region Variables
+
+        private CloudBlockBlob blob;
+        private string leaseId;
+        private Thread renewalThread;
+
+        // Used for the disposable interface
+        private bool disposed = false;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Create a new blob lock
+        /// </summary>
+        /// <param name="connectionString">A connection string to a azure storage account</param>
+        /// <param name="containerName">A name for the container</param>
+        /// <param name="blobName">A name for the blob</param>
+        public BlobLock(string connectionString, string containerName, string blobName)
+        {
+            // Get a storage account
+            CloudStorageAccount account = CloudStorageAccount.Parse(connectionString);
+
+            // Get a client
+            CloudBlobClient client = account.CreateCloudBlobClient();
+
+            // Get a reference to a cloud blob contaner
+            CloudBlobContainer container = client.GetContainerReference(containerName);
+
+            // Create a container if it does not exist
+            container.CreateIfNotExists();
+
+            // Get a blob object
+            blob = container.GetBlockBlobReference(blobName);
+
+            // Upload a blob if it does not exist
+            if (blob.Exists() == false)
+            {
+                blob.UploadFromStream(new MemoryStream());
+            }
+
+        } // End of the constructor
+
+        #endregion
+
+        #region Create methods
+
+        /// <summary>
+        /// Create a lock, wait if the lock not could be aquired
+        /// </summary>
+        /// <returns>A boolean that indicates if the lock is taken</returns>
+        public bool CreateOrWait()
+        {
+            // Try to aquire a blob lock
+            while (TryAcquireLease(this.blob) == false)
+            {
+                // Sleep times should be random
+                Random rnd = new Random();
+
+                // Sleep between 30 and 60 seconds
+                Thread.Sleep(rnd.Next(30000, 60000));
+            }
+
+            // Renew the lease until the work is done
+            RenewLease();
+
+            // Return a success boolean
+            return true;
+
+        } // End of the CreateOrWait method
+
+        /// <summary>
+        /// Create a lock, do not wait on the lock to be released
+        /// </summary>
+        /// <returns>A boolean that indicates if the lock is taken</returns>
+        public bool CreateOrSkip()
+        {
+            // Try to aquire a blob lock
+            bool success = TryAcquireLease(this.blob);
+
+            // Renew the lease if the lock is taken
+            if (success == true)
+            {
+                // Renew the lease until the work is done
+                RenewLease();
+            }
+
+            // Return the success boolean
+            return success;
+
+        } // End of the CreateOrSkip method
+
+        #endregion
+
+        #region Lease methods
+
+        /// <summary>
+        /// Try to get a lock
+        /// </summary>
+        /// <param name="blob">A reference to a blob</param>
+        /// <returns>A boolean that indicates if the lease was acquired</returns>
+        private bool TryAcquireLease(CloudBlockBlob blob)
+        {
+            try
+            {
+                // Acquire a lease on the blob
+                this.leaseId = blob.AcquireLease(TimeSpan.FromSeconds(60), null);
+
+                // Return true
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // There is a lock on the blob, return false
+                string exMessage = ex.Message;
+                return false;
+            }
+
+        } // End of the TryAcquireLease method
+
+        /// <summary>
+        /// Renew the lease every 40 seconds
+        /// </summary>
+        private void RenewLease()
+        {
+            renewalThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(40));
+                    blob.RenewLease(new AccessCondition { LeaseId = this.leaseId });
+                }
+            });
+            renewalThread.Start();
+
+        } // End of the RenewLease method
+
+        #endregion
+
+        #region Dispose methods
+
+        /// <summary>
+        /// Public implementation of Dispose pattern callable by consumers
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+
+        } // End of the Dispose method
+
+        /// <summary>
+        /// Protected implementation of Dispose pattern
+        /// </summary>
+        /// <param name="disposing">True or false</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                if (renewalThread != null)
+                {
+                    renewalThread.Abort();
+                    blob.ReleaseLease(new AccessCondition { LeaseId = this.leaseId });
+                    renewalThread = null;
+                }
+            }
+
+            // Indicate that the object is disposed
+            disposed = true;
+
+        } // End of the Dispose method
+
+        /// <summary>
+        /// The finalizer
+        /// </summary>
+        ~BlobLock()
+        {
+            Dispose(false);
+
+        } // End of the Finalizer
+
+        #endregion
+
+    } // End of the class
+
+} // End of the namespace
---
+++ TestProgram/TestBlobLock.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Annytab.AzureLock;
+
+/// <summary>
+/// This class is used to test blob locks
+/// </summary>
+[TestClass]
+public class TestBlobLock
+{
+    // Variables
+    private static string CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=XXX;AccountKey=XXX";
+
+    [TestMethod]
+    public void TestToCreateOrWaitForLock()
+    {
+        Task[] tasks = new[]
+        {
+            Task.Factory.StartNew(() => CreateOrWaitForLock(1)),
+            Task.Factory.StartNew(() => CreateOrWaitForLock(2)),
+            Task.Factory.StartNew(() => CreateOrWaitForLock(3)),
+            Task.Factory.StartNew(() => CreateOrWaitForLock(4))
+        };
+        Task.WaitAll(tasks);
+
+    } // End of the TestToCreateOrWaitForLock method
+
+    [TestMethod]
+    public void TestToCreateOrSkipLock()
+    {
+        Task[] tasks = new[]
+        {
+            Task.Factory.StartNew(() => CreateOrSkipLock(1)),
+            Task.Factory.StartNew(() => CreateOrSkipLock(2)),
+            Task.Factory.StartNew(() => CreateOrSkipLock(3)),
+            Task.Factory.StartNew(() => CreateOrSkipLock(4))
+        };
+        Task.WaitAll(tasks);
+
+    } // End of the TestToCreateOrSkipLock method
+
+    /// <summary>
+    /// Create or wait for a lock
+    /// </summary>
+    /// <param name="threadId">A thread id</param>
+    private void CreateOrWaitForLock(Int32 threadId)
+    {
+        // Use a blob lock, the lock is disposed by the using block
+        using (BlobLock blobLock = new BlobLock(CONNECTION_STRING, "locks", "locations.lck"))
+        {
+            // Do work inside a blob lock
+            if (blobLock.CreateOrWait() == true)
+            {
+                Debug.WriteLine("Thread " + threadId.ToString() + ": Has lock for 1 minute.");
+
+                // Sleep for 1 minute
+                Thread.Sleep(TimeSpan.FromMinutes(1));
+            }
+        }
+
+        //// Create a blob lock variable
+        //BlobLock blobLock = null;
+
+        //try
+        //{
+        //    // Create a blob lock
+        //    blobLock = new BlobLock(CONNECTION_STRING, "locks", "locations.lck");
+
+        //    // Do work inside a blob lock
+        //    if (blobLock.CreateOrWait() == true)
+        //    {
+        //        Debug.WriteLine("Thread " + threadId.ToString() + ": Has lock for 1 minute.");
+
+        //        // Sleep for 1 minute
+        //        Thread.Sleep(TimeSpan.FromMinutes(1));
+        //    }
+        //}
+        //catch (Exception ex)
+        //{
+        //    throw ex;
+        //}
+        //finally
+        //{
+        //    // Dispose of the blob lock
+        //    if (blobLock != null)
+        //    {
+        //        blobLock.Dispose();
+        //    }
+        //}
+
+    } // End of the CreateOrWaitForLock method
+
+    /// <summary>
+    /// Create a lock or skip if the lock is taken
+    /// </summary>
+    /// <param name="threadId">A thread id</param>
+    private void CreateOrSkipLock(Int32 threadId)
+    {
+        // Use a blob lock, the lock is disposed by the using block
+        using (BlobLock blobLock = new BlobLock(CONNECTION_STRING, "locks", "locations.lck"))
+        {
+            // Do work inside a blob lock
+            if (blobLock.CreateOrSkip() == true)
+            {
+                Debug.WriteLine("Thread " + threadId.ToString() + ": Has lock for 1 minute.");
+
+                // Sleep for 1 minute
+                Thread.Sleep(TimeSpan.FromMinutes(1));
+            }
+            else
+            {
+                Debug.WriteLine("Thread " + threadId.ToString() + ": Does not wait for the lock to be released.");
+            }
+        }
+
+        //// Create a blob lock variable
+        //BlobLock blobLock = null;
+
+        //try
+        //{
+        //    // Create a blob lock
+        //    blobLock = new BlobLock(CONNECTION_STRING, "locks", "locations.lck");
+
+        //    // Do work inside a blob lock
+        //    if (blobLock.CreateOrSkip() == true)
+        //    {
+        //        Debug.WriteLine("Thread " + threadId.ToString() + ": Has lock for 1 minute.");
+
+        //        // Sleep for 1 minute
+        //        Thread.Sleep(TimeSpan.FromMinutes(1));
+        //    }
+        //    else
+        //    {
+        //        Debug.WriteLine("Thread " + threadId.ToString() + ": Does not wait for the lock to be released.");
+        //    }
+        //}
+        //catch (Exception ex)
+        //{
+        //    throw ex;
+        //}
+        //finally
+        //{
+        //    // Dispose of the blob lock
+        //    if (blobLock != null)
+        //    {
+        //        blobLock.Dispose();
+        //    }
+        //}
+
+    } // End of the CreateOrSkipLock method
+
+} // End of the class
